4798e63b25c09dd61a28fd7d3177c1f5231d97f9
joelc <joel@redscientific.com>
2021-11-10 09:57:22 -0800

    Fixed make file to build without unnecessary items.

--- make.bat
+++ make.bat
@@ -1,13 +1,12 @@
 pyinstaller ^
-	--clean ^
 	--onefile ^
-	--noconfirm ^
 	--noconsole ^
-	--add-data="RSLogger\img\pause.png;RSLogger\img" ^
-	--add-data="RSLogger\img\rs_icon.ico;RSLogger\img" ^
+	--clean ^
+	--noconfirm ^
+	--add-data="rs_icon.ico;." ^
 	--add-data="RSLogger\img\questionmark_15.png;RSLogger\img" ^
 	--add-data="RSLogger\img\record.png;RSLogger\img" ^
-	--add-data="RSLogger\img\refresh_15.png;RSLogger\img" ^
-	--name="RSLogger" ^
-	--icon=RSLogger\img\rs_icon.ico ^
+	--add-data="RSLogger\img\pause.png;RSLogger\img" ^
+	--icon=rs_icon.ico ^
+	--name="RS_Logger" ^
 	main.py
