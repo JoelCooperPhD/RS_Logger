82d797967b242eb19acc51100dcc9088b1fac151
joelc <joel@redscientific.com>
2021-11-19 20:02:53 -0800

    version 0.1.4

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from multiprocessing import freeze_support
 from RSLogger.UserInterface import UserInterface
 from RSLogger.HardwareInterface import HardwareInterface
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 # Inter-thread communication queue dictionary
 queues = {'main': SimpleQueue(),
