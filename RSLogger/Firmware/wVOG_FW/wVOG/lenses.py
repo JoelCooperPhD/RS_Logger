362a807948aa37d43070d4ab5971faf9150b939b
Red Scientific <33227781+redscientific@users.noreply.github.com>
2022-03-04 10:11:41 -0800

    Added lens fading and cleaned up class

--- RSLogger/Firmware/wVOG_FW/wVOG/lenses.py
+++ RSLogger/Firmware/wVOG_FW/wVOG/lenses.py
@@ -1,44 +1,76 @@
-import uasyncio as asyncio
-import utime as time
-import random
 import pyb
+import time
 
 
 class Lenses:
     def __init__(self):
-        self._run_AC = True
+        # Lens controls
+        self._lens_a = pyb.Pin(pyb.Pin.board.X9, pyb.Pin.OUT, value=0)
+        self._lens_b = pyb.Pin(pyb.Pin.board.X10, pyb.Pin.OUT, value=0)
 
-        self._toggle_1 = pyb.Pin(pyb.Pin.board.Y3, pyb.Pin.OUT)
-        self._toggle_2 = pyb.Pin(pyb.Pin.board.Y4, pyb.Pin.OUT)
+        # Transformer controls for legs 1 and 2
+        self._flip_flop = True
+        self._pwm_vals = [0.0, 100.0]
+        self._pwm_vals[self._flip_flop]
 
-        self._lens_a = pyb.Pin(pyb.Pin.board.X9, pyb.Pin.OUT)
-        self._lens_b = pyb.Pin(pyb.Pin.board.X10, pyb.Pin.OUT)
-        
-        asyncio.create_task(self._arm_xformer())
+        tim = pyb.Timer(3, freq=1000)
+        self._toggle_1 = tim.channel(1, pyb.Timer.PWM, pin=pyb.Pin('Y3'))
+        self._toggle_1.pulse_width_percent(self._pwm_vals[0])
 
-    async def _arm_xformer(self):
-        val = True
-        while True:
-            if self._run_AC:
-                self._toggle_1.value(val)
-                self._toggle_2.value(not val)
-                val = not val
-            else:
-                self._toggle_1.value(0)
-                self._toggle_2.value(0)
-            await asyncio.sleep_ms(5)
+        tim = pyb.Timer(3, freq=1000)
+        self._toggle_2 = tim.channel(2, pyb.Timer.PWM, pin=pyb.Pin('Y4'))
+        self._toggle_2.pulse_width_percent(self._pwm_vals[1])
 
+        # Transformer toggle timer - toggles each leg at a set interval
+        self._toggle_timer = pyb.Timer(4, freq=120)
+        self._toggle_timer.callback(self._toggle_xformer)
+
+    def _toggle_xformer(self, tmr):
+        # This is a timer based callback from timer 4
+        self._toggle_1.pulse_width_percent(self._pwm_vals[self._flip_flop])
+        self._toggle_2.pulse_width_percent(self._pwm_vals[not self._flip_flop])
+        self._flip_flop = not self._flip_flop
+
+    # Lens opacity when switch on via "clear" method
+    @property
+    def on_opacity(self):
+        return self._pwm_vals[1]
+
+    @on_opacity.setter
+    def on_opacity(self, val):
+        if val > 100:
+            val = 100
+        self._pwm_vals[1] = val
+
+    # Lens opacity when switch off via "opaque" method
+    @property
+    def off_opacity(self):
+        return self._pwm_vals[0]
+
+    @off_opacity.setter
+    def off_opacity(self, val):
+        if val < 0:
+            val = 0
+        self._pwm_vals[0] = val
+
+    ###################################################
+    # Lens control
     def clear(self, lenses=['a', 'b']):
-        self._run_AC = True
-        self._lens_a.value(1 if 'a' in lenses else 0)
-        self._lens_b.value(1 if 'b' in lenses else 0)
-        
+        if 'a' in lenses:
+            self._lens_a.value(1)
+        if 'b' in lenses:
+            self._lens_b.value(1)
+        return time.ticks_ms()
 
     def opaque(self, lenses=['a', 'b']):
-        self._run_AC = False
-        self._toggle_1.value(0)
-        self._toggle_2.value(0)
-        self._lens_a.value(0 if 'a' in lenses else 1)
-        self._lens_b.value(0 if 'b' in lenses else 1)
+        self._toggle_timer.callback(None)
+
+        if 'a' in lenses:
+            self._lens_a.value(0)
+        if 'b' in lenses:
+            self._lens_b.value(0)
+
+        self._toggle_timer.callback(self._toggle_xformer)
+        return time.ticks_ms()
 
 
