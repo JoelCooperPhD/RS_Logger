31ea1d396bd477731e1a991f455db146bd0fd6ce
Red Scientific <33227781+redscientific@users.noreply.github.com>
2022-03-10 09:45:51 -0800

    Add ABTimerAccumulator

--- RSLogger/Firmware/wVOG_FW/wVOG/timers.py
+++ RSLogger/Firmware/wVOG_FW/wVOG/timers.py
@@ -15,13 +15,15 @@ class Stopwatch:
             self._tic = ticks_ms()
         elif self._precision == 'us':
             self._tic = ticks_us()
-            
+
     def read(self):
         if self._running:
             if self._precision == 'ms':
                 return ticks_diff(ticks_ms(), self._tic)
             elif self._precision == 'us':
                 return ticks_diff(ticks_us(), self._tic)
+        else:
+            return 0
 
     def stop(self, in_t=None):
         if self._running:
@@ -36,12 +38,15 @@ class Stopwatch:
                     toc = ticks_us()
 
             return ticks_diff(toc, self._tic)
+        else:
+            return 0
 
 
 class Countdown:
     def __init__(self, callback, duration=None):
         self._duration = duration
         self._callback = callback
+        self._stop = False
         self.run = True
 
     @property
@@ -54,12 +59,65 @@ class Countdown:
             self._duration = val
 
     async def start(self, duration=None):
+        self._stop = False
         if duration:
             self._duration = duration
         start_t = ticks_ms()
         while self.run:
             elapsed = ticks_diff(ticks_ms(), start_t)
-            if elapsed >= self._duration:
-                self._callback(elapsed)
-                break
-            await asyncio.sleep(0)
+
+
+class ABTimeAccumulator:
+    def __init__(self):
+        # point time variables
+        self._start_ms = 0
+        self._stop_ms = 0
+        self._change_ms = 0
+
+        # time accumulators
+        self._accumulator = [0, 0]
+
+        # state tracker
+        self._i = False
+
+    def start(self, start_closed=False):
+        now = ticks_ms()
+        self._start_ms = now
+        self._toggle_ms = now
+
+        self._accumulator = [0, 0]
+        self._data = ['a', 0, 0, 0]
+
+        self._i = start_closed
+        return self._state(now)
+
+    def stop(self):
+        now = ticks_ms()
+        self._update_accumulator(now)
+
+        return self._state(now, stop=True)
+
+    def toggle(self):
+        now = ticks_ms()
+
+        self._update_accumulator(now)
+
+        self._toggle_ms = now
+        self._i = not self._i
+
+        return self._state(now)
+
+    def _update_accumulator(self, now):
+        elapsed = ticks_diff(now, self._toggle_ms)
+        self._accumulator[self._i] += elapsed
+
+    def _state(self, now, stop=False):
+        if stop:
+            self._data[0] = 'x'
+        else:
+            self._data[0] = 'b' if self._i else 'a'
+
+        self._data[1] = ticks_diff(now, self._start_ms)
+        self._data[2:4] = self._accumulator
+
+        return self._data
