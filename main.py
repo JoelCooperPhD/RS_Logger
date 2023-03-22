ffb58a5754aec53f1823fa1b36434e4fa6430a6b
Red Scientific <33227781+redscientific@users.noreply.github.com>
2023-03-22 10:43:54 -0600

    bumped to version 1.2.1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from multiprocessing import freeze_support
 from RSLogger.hardware_io import hi_controller
 from RSLogger.user_interface import ui_controller
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 # Inter-thread communication queue dictionary
 queues = {'q_2_hi': SimpleQueue(),
